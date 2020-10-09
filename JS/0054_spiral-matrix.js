4f58cd591a8d4abdfb043b4815898f7d6f42686f
Bhushan Mahangare <bhushan_mahangare@wifi-soft.com>
2020-10-09 10:05:04 +0530

    Prob0054 Spiral Matrix

---
+++ JS/0054_spiral-matrix.js
@@ -0,0 +1,81 @@
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 54. Spiral Matrix
+ * 
+ * Medium
+ * 
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in 
+ * spiral order.
+ * 
+ * Example 1:
+ * Input:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 4, 5, 6 ],
+ *  [ 7, 8, 9 ]
+ * ]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ * 
+ * Example 2:
+ * Input:
+ * [
+ *  [1, 2, 3, 4],
+ *  [5, 6, 7, 8],
+ *  [9,10,11,12]
+ * ]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * 
+ * ** =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+ /**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+//
+var spiralOrder = function(matrix) {
+    var result = [];
+  
+    if (matrix === null || matrix.length === 0 || matrix[0].length === 0) {
+      return result;
+    }
+  
+    var rows = matrix.length;
+    var cols = matrix[0].length;
+  
+    var x = 0;
+    var y = 0;
+  
+    while (rows > 0 && cols > 0) {
+      if (rows === 1) {
+        for (var i = 0; i < cols; i++) {
+          result.push(matrix[x][y++]);
+        }
+        break;
+      } else if (cols === 1) {
+        for (i = 0; i < rows; i++) {
+          result.push(matrix[x++][y]);
+        }
+        break;
+      }
+  
+      for (i = 0; i < cols - 1; i++) {
+        result.push(matrix[x][y++]);
+      }
+      for (i = 0; i < rows - 1; i++) {
+        result.push(matrix[x++][y]);
+      }
+      for (i = 0; i < cols - 1; i++) {
+        result.push(matrix[x][y--]);
+      }
+      for (i = 0; i < rows - 1; i++) {
+        result.push(matrix[x--][y]);
+      }
+  
+      x++;
+      y++;
+      cols -= 2;
+      rows -= 2;
+    }
+  
+    return result;
+  };
+  
---
+++ Python/0054_spiral-matrix.py
@@ -0,0 +1,58 @@
+"""=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 54. Spiral Matrix
+ * 
+ * Medium
+ * 
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in 
+ * spiral order.
+ * 
+ * Example 1:
+ * Input:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 4, 5, 6 ],
+ *  [ 7, 8, 9 ]
+ * ]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ * 
+ * Example 2:
+ * Input:
+ * [
+ *  [1, 2, 3, 4],
+ *  [5, 6, 7, 8],
+ *  [9,10,11,12]
+ * ]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * 
+ * ** =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="""
+
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if not matrix:
+        	return []
+
+        R, C = len(matrix), len(matrix[0])
+        dr = [0, 1, 0, -1]
+        dc = [1, 0, -1, 0]
+
+        result = []
+        seen = [[False]*C for _ in range(R)]
+        row = 0
+        col = 0
+        di = 0
+        for _ in range(R*C):
+        	result.append(matrix[row][col])
+        	seen[row][col] = True
+        	rr, cc = row + dr[di], col + dc[di]
+        	if 0 <= rr < R and 0 <= cc < C and not seen[rr][cc]:
+        		row, col = rr, cc
+        	else:
+        		di = (di+1)%4
+        		row, col = row + dr[di], col + dc[di]
+
+        return result
